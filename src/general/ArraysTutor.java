bfe337dad6ed9dc9e944893c30e7abc68b4837ac
kozakw <94359492+kozakw@users.noreply.github.com>
2021-11-15 14:32:49 +0100

    Update ArraysTutor.java

--- src/general/ArraysTutor.java
+++ src/general/ArraysTutor.java
@@ -3,5 +3,12 @@ package general;
 public class ArraysTutor {
     public static void main(String[] args) {
         int [] x = new int[5];
+
+        x[0] = 10;
+        x[1] = 12;
+
+        for(int i = 0; i < x.length; i++){
+            x[i] = 10 + i;
+        }
     }
 }

f8cae7a638cef3f7f32b92f446c036fce0080caa
kozakw <wiktor.kozak@hotmail.com>
2021-11-15 11:09:20 +0100

    Added InheritanceMain.java

---
+++ src/object_oriented_programming/inheritance/InheritanceMain.java
@@ -0,0 +1,12 @@
+package object_oriented_programming.inheritance;
+
+public class InheritanceMain {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(2,2, 6);
+
+        Circle c2 = new Circle();
+
+
+    }
+
+}
